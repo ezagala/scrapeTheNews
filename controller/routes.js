a48a1d796fd5f5b3d450b4ea1e00a221eec2b477
Eric <ejzagala@gmail.com>
2018-06-22 19:32:50 -0600

    Articles scrapped and stored

--- controller/routes.js
+++ controller/routes.js
@@ -4,88 +4,112 @@ const cheerio = require("cheerio");
 const db = require("../models");
 const moment = require("moment");
 
-module.exports =  app => {
+module.exports = app => {
 
   // Get route that scrapes the specified site for the content 
   app.get('/scrape', (req, res) => {
 
     const date = moment().format("YYYY/MM/DD");
 
-    axios.get(`https://www.democracynow.org/${date}/headlines/`).then( response => {
+    axios.get(`https://www.democracynow.org/${date}/headlines/`).then(function (response) {
       // Define shorthand for cheerio 
       const $ = cheerio.load(response.data);
 
-      $(".headline h2").each( (i, element) => {
-        const result = {}; 
+      // Traverse each element with the headline class
+      $(".headline").each(function (i, element) {
 
-        result.headline = $(this).text();
+        // Declare object to store each piece
+        const result = {};
 
-      })
+        // Grab & store the piece's contents 
+        result.headline = $(this)
+          .children("h2")
+          .text();
 
+        result.imgSrc = $(this)
+          .children("img")
+          .attr("src");
 
+        result.summary = $(this)
+          .children(".headline_body")
+          .children(".headline_summary")
+          .children("p")
+          .text();
 
+          result.date = $(this)
+            .children(".news_label")
+            .children(".date")
+            .text()
 
+        console.log("The result is: ", result)
 
-      // Target the elements of intersted and save them off. This logic will iterate through each such element on the page, i.e. the code in this block will loop. 
-      // Maybe into a constructor? It could have methods that could deal w/ adding, modifying and deleting comments 
-
-      // Update the database with the new piece
-
-      console.log("The response was", $);
-      res.send("Woopty Woop!"); 
+        // Create a new Article using the `result` object built from scraping
+        db.Article.create(result)
+          .then(function (dbArticle) {
+            // View the added result in the console
+            console.log(dbArticle);
+          })
+          .catch(function (err) {
+            // If an error occurred, send it to the client
+            return res.json(err);
+          });
 
     });
-  });
 
-  // Route for getting all Articles from the db
-  app.get("/articles", function (req, res) {
-    // Grab every document in the Articles collection
-    db.Article.find({})
-      .then(function (dbArticle) {
-        // If we were able to successfully find Articles, send them back to the client
-        res.json(dbArticle);
-      })
-      .catch(function (err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
+    res.send("Woopty Woop!");
 
-  // Route for grabbing a specific Article by id, populate it with it's note
-  app.get("/articles/:id", function (req, res) {
-    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-    db.Article.findOne({ _id: req.params.id })
-      // ..and populate all of the notes associated with it
-      .populate("note")
-      .then(function (dbArticle) {
-        // If we were able to successfully find an Article with the given id, send it back to the client
-        res.json(dbArticle);
-      })
-      .catch(function (err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
   });
+});
 
-  // Route for saving/updating an Article's associated Note
-  app.post("/articles/:id", function (req, res) {
-    // Create a new note and pass the req.body to the entry
-    db.Note.create(req.body)
-      .then(function (dbNote) {
-        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-      })
-      .then(function (dbArticle) {
-        // If we were able to successfully update an Article, send it back to the client
-        res.json(dbArticle);
-      })
-      .catch(function (err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
+// Route for getting all Articles from the db
+app.get("/articles", function (req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function (dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function (req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function (dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function (req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function (dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function (dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
 
 
--- models/Article.js
+++ models/Article.js
@@ -1,23 +1,25 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// Save a reference to the Schema constructor
-var Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
-
-// Would be nice to add article byline, image and content 
-
-var ArticleSchema = new Schema({
+const ArticleSchema = new Schema({
   // `title` is required and of type String
-  title: {
+  headline: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  // `link` is required and of type String
+  imgSrc: {
     type: String,
     required: true
   },
-  // `link` is required and of type String
-  link: {
-    type: String,
-    required: true
+  summary: {
+    type: String, 
+    required: false
+  },
+  date: {
+    type: String
   },
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
@@ -29,7 +31,7 @@ var ArticleSchema = new Schema({
 });
 
 // This creates our model from the above schema, using mongoose's model method
-var Article = mongoose.model("Article", ArticleSchema);
+const Article = mongoose.model("Article", ArticleSchema);
 
 // Export the Article model
 module.exports = Article;
